e6dc79976fb00d63adf10322bbeaa6aa7a068bb4
Ocholi Joseph <ocholijoseph@gmail.com>
2022-12-09 00:58:17 +0100

    commits

--- 0x00-hello_world/4-puts.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-/* Main - Entry Point */
-int main(void) {
-    /*Return: Always 0 (Success)*/
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
-}
