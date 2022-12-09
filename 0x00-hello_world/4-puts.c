c92fc0b0b9f239e1c74d7195eb4c4209052b6fa0
Ocholi Joseph <ocholijoseph@gmail.com>
2022-12-09 04:44:51 +0100

    Go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * Main - main block
+ * main - Entry point
  *
- * Return: 0
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like buiding a multilingual puzzle");
 	return (0);
 }
