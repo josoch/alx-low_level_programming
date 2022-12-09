f0f34bf9ccb40c8c20934f78f6b3fe44062e4c4b
Ocholi Joseph <ocholijoseph@gmail.com>
2022-12-08 17:25:21 -0800

    Co

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -S $CFILE
+n(void)
+{
+	    int a;
+	        long int b;
+		    long long int c;
+		        char d;
+			    float f;
+			        printf("Size of a char %lu byte(s)\n, (unsigned long)sizeof(d));
+				    printf("Size of an int %lu byte(s)\n, (unsigned long)sizeof(a));
+				        printf("Size of a long int %lu byte(s)\n, (unsigned long)sizeof(c));
+					    printf("Size of a float %lu byte(s)\n, (unsigned long)sizeof(f));
+					        return (0);
+}
