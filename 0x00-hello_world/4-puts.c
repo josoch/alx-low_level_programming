86e27c887d760c96e39b69458403c5be9d05e9d3
Ocholi Joseph <ocholijoseph@gmail.com>
2022-12-09 04:34:24 +0100

    Going

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * Main - main block
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle,");
 	return (0);
 }
