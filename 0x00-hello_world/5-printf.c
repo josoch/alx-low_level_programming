48dfa8abddada3e452169bc8c6dd438c3302a5c2
Ocholi Joseph <ocholijoseph@gmail.com>
2022-12-09 04:51:36 +0100

    We are done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  *
- * Description: 'Printf'
+ * Description: 'the program's description'
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
