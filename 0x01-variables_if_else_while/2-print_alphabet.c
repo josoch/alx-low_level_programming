57686e4e59b608f0865d365eac240bd849a3a0f2
Joseph Ocholi <ocholijoseph@gmail.com>
2022-12-11 01:53:45 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - initiating betty
+ * main - alphabet in lowercase
  *
- * Description: main
+ * Description: Playing play the alphabet game
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+	int n, l;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	n = 97;
+	l = 65;
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	while (l < 91)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(10);
 	return (0);
 }
