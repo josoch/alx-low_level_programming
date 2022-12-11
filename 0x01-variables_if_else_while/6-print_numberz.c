a64174b9ce349349db65576505238335a679638e
Joseph Ocholi <ocholijoseph@gmail.com>
2022-12-11 07:37:38 -0800

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,25 @@
-#include <unistd.h>
+/*
+ * File: 6-print_numberz.c
+ * Authior: Ocholi Joseph
+ */
+
+#include <stdio.h>
+
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
  *
- *
- * Return: Always 1.
+ * Return: Always 0.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
 }
+
