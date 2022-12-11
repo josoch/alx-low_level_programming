736d51dc8dfb6da2b3c52cb113414beb315387a5
Joseph Ocholi <ocholijoseph@gmail.com>
2022-12-11 08:09:00 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,39 @@
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
+/*
+ * File: 102-print_comb5.c
+ * Author: Ocholi Joseph
+ */
+
+#include <stdio.h>
+
 /**
- * main - initiating betty
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
  *
- * Description: main
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int n;
+	int num1, num2;
 
-	srand(time());
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		print("%d is negative\n", n);
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
