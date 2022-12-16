f3adcd0001ac52eecc0dbeb2e722de42e20676c8
Joseph Ocholi <ocholijoseph@gmail.com>
2022-12-15 20:10:13 -0800

    Coomit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,30 +12,25 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int num;
-
-	for (num = 1; num <= 100; num++)
+	for (int i = 1; i <= 100; i++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
-
-		else if ((num % 3) == 0)
-			printf("Fizz");
-
-		else if ((num % 5) == 0)
-			printf("Buzz");
-
-		else
-			printf("%d", num);
-
-		if (num == 100)
-			continue;
-		printf(" ");
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		} else {
+			printf("%d ", i);
+		}
 	}
-
 	printf("\n");
-
-	return (0);
+	return 0;
 }
+
