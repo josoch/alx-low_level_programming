79ec585bf3436a2b6cbda8f75431adab5682b086
Joseph Ocholi <ocholijoseph@gmail.com>
2022-12-15 20:27:59 -0800

    Commit it

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,10 +10,12 @@
  */
 void print_numbers(void)
 {
-    for (int i = 0; i < 10; i++)
-    {
-        _putchar(i + '0');
-    }
-    _putchar('\n');
+	int i;
+
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
 
