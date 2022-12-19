4f2458e2507f767f4fd4c30c379578ac5540090e
Josoch <ocholijoseph@gmail.com>
2022-12-19 10:32:19 +0000

    Task 4: I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -5,11 +5,10 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	char *str;
+int main(void) {
+  char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	return (0);
+  str = "Holberton!";
+  print_rev(str);
+  return (0);
 }
