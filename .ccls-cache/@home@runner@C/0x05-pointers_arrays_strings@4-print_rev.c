9ad1ab4d6f68e00b87eda23dd3beb0da813a2374
Josoch <ocholijoseph@gmail.com>
2022-12-19 10:29:12 +0000

    Task 4: I can only go one way. I've not got a reverse gear

--- .ccls-cache/@home@runner@C/0x05-pointers_arrays_strings@4-print_rev.c
+++ .ccls-cache/@home@runner@C/0x05-pointers_arrays_strings@4-print_rev.c
@@ -9,16 +9,14 @@
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void print_rev(char *s)
-{
-	int len = 0, index;
+void print_rev(char *s) {
+  int len = 0, index;
 
-	while (s[index++])
-		len++;
+  while (s[index++])
+    len++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+  for (index = len - 1; index >= 0; index--)
+    _putchar(s[index]);
 
-	_putchar('\n');
+  _putchar('\n');
 }
-
