7a126eae446544143d5705fa4f0cd3499f57b1f9
Ocholi Joseph <ocholijoseph@gmail.com>
2023-01-10 01:04:34 -0800

    Effecting malloc, free task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,10 +5,11 @@
  * File: main.h
  * Author: Ocholi Joseph
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x0A-malloc_free directory.
+ *       used in the 0x0B-malloc_free directory.
  */
 
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
